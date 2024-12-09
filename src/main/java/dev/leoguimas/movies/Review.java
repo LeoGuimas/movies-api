43ee189dfcee59f856bb6d64f989ca4c3200f1c8
LeoGuimas <gleo.dev0@gmail.com>
2024-12-09 15:42:42 -0300

    Add Review model, repository, and service for managing movie reviews

--- src/main/java/dev/leoguimas/movies/Review.java
+++ src/main/java/dev/leoguimas/movies/Review.java
@@ -1,7 +1,9 @@
 package dev.leoguimas.movies;
 
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
+    @Id
     private ObjectId id;
     private String body;
 
+    public Review(String body) {
+        this.body = body;
+    }
+
 }
+
