d63daacdb2a524863e175713a9369dcbdc4c26d4
LeoGuimas <gleo.dev0@gmail.com>
2024-12-16 11:21:00 -0300

    Update CORS allowed origins for enhanced security

--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Aplica CORS a todas as rotas
-                .allowedOrigins("http://localhost:3000", "https://6397-2804-a80-1041-e75c-d502-b5b3-2758-532b.ngrok-free.app")
+                .allowedOrigins("http://localhost:3000", "https://4af3-206-42-59-176.ngrok-free.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);  // Caso esteja lidando com cookies ou autenticação
